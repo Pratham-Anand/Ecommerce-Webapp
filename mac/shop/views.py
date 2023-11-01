dd438670e5b2a9d2ff41163de32cb8d62df54262
Pratham-Anand <anandpratham13@gmail.com>
2023-11-01 07:10:34 +0530

    solved tracker bug

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -76,6 +76,7 @@ def searchMatch(query, item):
     else:
         return False
     
+
 def tracker(request):
     if request.method=="POST":
         orderId = request.POST.get('orderId', '')
@@ -87,14 +88,15 @@ def tracker(request):
                 updates = []
                 for item in update:
                     updates.append({'text': item.update_desc, 'time': item.timestamp})
-                    response = json.dumps([updates, order[0].items_json],default=str)
+                    response = json.dumps({"status":"success", "updates": updates, "itemsJson": order[0].items_json}, default=str)
                 return HttpResponse(response)
             else:
-                return HttpResponse('{}')
+                return HttpResponse('{"status":"noitem"}')
         except Exception as e:
-            return HttpResponse('{}')
+            return HttpResponse('{"status":"error"}')
 
     return render(request, 'shop/tracker.html')
+
 
 
 def productview(request,myid):
